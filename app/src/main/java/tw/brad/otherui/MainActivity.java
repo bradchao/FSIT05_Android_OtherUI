701894c7c434490de8742511c60c09cb26c65e60
Brad <brad@brad.tw>
2018-11-04 10:48:46 +0800

    v1

---
+++ app/src/main/java/tw/brad/otherui/MainActivity.java
@@ -0,0 +1,88 @@
+package tw.brad.otherui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import co.ceryle.segmentedbutton.SegmentedButtonGroup;
+
+public class MainActivity extends AppCompatActivity {
+    private CheckBox opt1;
+    private RadioGroup years;
+    private ToggleButton tbtn;
+    private SegmentedButtonGroup sbg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        opt1 = findViewById(R.id.opt1);
+        opt1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(MainActivity.this, "OK", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+
+        years = findViewById(R.id.years);
+
+        years.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.year1:
+                        Toast.makeText(MainActivity.this, "20-", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    case R.id.year2:
+                        Toast.makeText(MainActivity.this, "20-40", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    case R.id.year3:
+                        Toast.makeText(MainActivity.this, "40-60", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    case R.id.year4:
+                        Toast.makeText(MainActivity.this, "60+", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                }
+            }
+        });
+
+        tbtn = findViewById(R.id.tbtn);
+        tbtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this,
+                        (isChecked?"開":"關"),
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        sbg = findViewById(R.id.sbg);
+        sbg.setOnPositionChangedListener(new SegmentedButtonGroup.OnPositionChangedListener() {
+            @Override
+            public void onPositionChanged(int position) {
+                Toast.makeText(MainActivity.this,
+                        "pos: " + position,
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+
+
+
+    }
+}
