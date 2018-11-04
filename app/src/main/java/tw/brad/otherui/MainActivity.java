7a6374fd00318e7072ec0e2a3c48866044b7e0ef
Brad <brad@brad.tw>
2018-11-04 11:23:48 +0800

    v2

--- app/src/main/java/tw/brad/otherui/MainActivity.java
+++ app/src/main/java/tw/brad/otherui/MainActivity.java
@@ -1,13 +1,22 @@
 package tw.brad.otherui;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 import co.ceryle.segmentedbutton.SegmentedButtonGroup;
 
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup years;
     private ToggleButton tbtn;
     private SegmentedButtonGroup sbg;
+    private TextView newdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +92,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        newdate = findViewById(R.id.newdate);
 
+
+    }
+
+    public void chDate(View view) {
+        Calendar cal = Calendar.getInstance();
+        int yy = cal.get(Calendar.YEAR);
+        int mm = cal.get(Calendar.MONTH);
+        int dd = cal.get(Calendar.DAY_OF_MONTH);
+
+        new DatePickerDialog(this,
+                android.app.AlertDialog.THEME_DEVICE_DEFAULT_DARK,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        newdate.setText(year + "-" + (month+1) + "-" + dayOfMonth);
+                    }
+                },
+                yy, mm, dd).show();
 
     }
 }
